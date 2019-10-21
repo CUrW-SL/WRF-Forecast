5387ae01958dd3bfaca3b0f897c2a2c0c5422620
hasitha <hasithadkr7@gmail.com>
2019-10-21 06:31:49 +0530

    get gfs file list for a given gfs hour

--- forecast.py
+++ forecast.py
@@ -1,12 +1,19 @@
 from ftplib import FTP
 
 
+def get_file(ftp, filename):
+    try:
+        ftp.retrbinary('RETR ' + filename, open(filename, 'wb').write)
+    except:
+        print('Error')
+
+
 def connect_gfs_ftp_server():
     ftp = FTP('ftp.ncep.noaa.gov')
     ftp.login()
     print(ftp.pwd())
+    ftp.cwd('/pub/data/nccf/com/gfs/prod/gfs.20191020/18/')
     print(ftp.retrlines('LIST'))
-    ftp.cwd('data/nccf/com/gfs/prod')
 
 
 if __name__ == '__main__':
